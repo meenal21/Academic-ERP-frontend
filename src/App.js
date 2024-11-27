48b4ae88c74731a966adab00e42388ebe6c5828d
Meenal Jain <mpbjain@gmail.com>
2024-11-28 00:51:50 +0530

    Adding the RoleSelect Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import LoginForm from './AdminLogin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/AdminLogin';
+import RoleSelect from './components/RoleSelect';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LoginForm />
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <LoginForm />
+    //   </header>
+    // </div>
+
+    //We create a main router - which  a
+    <Router>
+      <Routes>
+        <Route path='/' element={<RoleSelect />}/>
+        <Route path='/login' element={<LoginForm/>} />
+      </Routes>
+    </Router>
   );
 }
 
